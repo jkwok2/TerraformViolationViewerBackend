5a862bbd6a13938c9eea1f139c4661d62cb8b2b3
naztazeem <nazish.tazeem@gmail.com>
2021-11-22 16:27:28 -0800

    updated sql

--- functions/routes/setupdb.sql
+++ functions/routes/setupdb.sql
@@ -18,7 +18,8 @@ create table Violations(violationId int primary key not null auto_increment,
                         filePath varchar(120) not null,
                         lineNumber int not null,
                         violationType varchar(45) not null,
-                        violationTime datetime not null
+                        violationTime datetime not null,
+                        dateFound date not null
 );
 
 create table Rules(ruleId int primary key not null auto_increment,
@@ -30,21 +31,17 @@ create table Rules(ruleId int primary key not null auto_increment,
                    description varchar(255),
                    dateAdded date not null,
                    content TEXT not null
+
 );
 
 insert into Users (userId, username, email, givenName, familyName, userRole) values ('111561841222565942402', 'HSBC Violation Viewer', 'hsbcviolationviewer@gmail.com', 'HSBC Violation Viewer', 'HSBC Violation Viewer', 'admin');
 insert into Users (userId, username, email, givenName, familyName, userRole) values ('105989777376658273094', 'GokceDilek', 'cpsc319.fall2021@gmail.com', 'Gokce', 'Dilek', 'base');
 
-insert into Violations (userId, repoId, prId, filePath, lineNumber, violationType, violationTime) values ('111561841222565942402', 'R123', '31234', './testfilepath', '123', 'Violation Type 1', '2020-08-24 13:45:23');
-insert into Violations (userId, repoId, prId, filePath, lineNumber, violationType, violationTime) values ('111561841222565942402', 'R123', '31234', './testfilepath', '123', 'Violation Type 2', '2021-01-16 11:00:00');
-insert into Violations (userId, repoId, prId, filePath, lineNumber, violationType, violationTime) values ('111561841222565942402', 'R124', '31235', './testfilepath', '123', 'Violation Type 1', '2021-02-13 16:06:56');
-insert into Violations (userId, repoId, prId, filePath, lineNumber, violationType, violationTime) values ('105989777376658273094', 'R125', '31236', './testfilepath', '123', 'Violation Type 3', '2021-06-13 14:34:56');
-insert into Violations (userId, repoId, prId, filePath, lineNumber, violationType, violationTime) values ('105989777376658273094', 'R124', '31237', './testfilepath', '123', 'Violation Type 2', '2021-02-13 12:42:56');
-insert into Violations (userId, repoId, prId, filePath, lineNumber, violationType, violationTime) values ('105989777376658273094', 'R126', '31237', './testfilepath', '123', 'Violation Type 4', '2021-04-13 12:21:56');
+insert into Violations (userId, repoId, prId, filePath, lineNumber, violationType, violationTime, dateFound) values ('111561841222565942402', 'R123', '31234', './testfilepath', '123', 'Violation Type 1', '2020-08-24 13:45:23', '2020-08-24');
+insert into Violations (userId, repoId, prId, filePath, lineNumber, violationType, violationTime, dateFound) values ('111561841222565942402', 'R123', '31234', './testfilepath', '123', 'Violation Type 2', '2021-01-16 11:00:00', '2015-11-16');
+insert into Violations (userId, repoId, prId, filePath, lineNumber, violationType, violationTime, dateFound) values ('111561841222565942402', 'R124', '31235', './testfilepath', '123', 'Violation Type 1', '2021-02-13 16:06:56', '2017-05-12');
+insert into Violations (userId, repoId, prId, filePath, lineNumber, violationType, violationTime, dateFound) values ('105989777376658273094', 'R125', '31236', './testfilepath', '123', 'Violation Type 3', '2021-06-13 14:34:56', '2012-02-19');
+insert into Violations (userId, repoId, prId, filePath, lineNumber, violationType, violationTime, dateFound) values ('105989777376658273094', 'R124', '31237', './testfilepath', '123', 'Violation Type 2', '2021-02-13 12:42:56', '2020-05-20');
+insert into Violations (userId, repoId, prId, filePath, lineNumber, violationType, violationTime, dateFound) values ('105989777376658273094', 'R126', '31237', './testfilepath', '123', 'Violation Type 4', '2021-04-13 12:21:56', '2020-08-21');
 
-insert into Rules (fileId, awsresource, severity, category, description, dateAdded, content, status) values ('F1','aws_elb','S1', 'C1', 'abc', '2021-06-13', 'content of F1', 'active');
-insert into Rules (fileId, awsresource, severity, category, description, dateAdded, content, status) values ('F2', 'aws_s3_bucket_public_access_block', 'S2', 'C2', 'def', '2020-10-12', 'content of F2','active');
-insert into Rules (fileId, awsresource, severity, category, description, dateAdded, content, status) values ('F3','aws_instance', 'S3', 'C1', 'ghi', '2017-02-21', 'content of F3', 'active');
-insert into Rules (fileId, awsresource, severity, category, description, dateAdded, content, status) values ('F4', 'aws_docdb_cluster_parameter_group', 'S4', 'C1', 'jkl', '2018-04-18', 'content of F4', 'active');
-
-
+insert into Rules (fileId, awsresource, severity, category, status, description, dateAdded, content) values ()
