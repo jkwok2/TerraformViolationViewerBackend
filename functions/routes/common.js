1e6d2e803adce4d11a18fd46d919868397da4a37
gokcedilek <gokcedilek99@gmail.com>
2021-11-25 22:31:17 -0800

    update to async await

--- functions/routes/common.js
+++ functions/routes/common.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 const config = {
   connectionLimit: 100,
   host: 'database-1.cphcofv6hw3s.us-east-1.rds.amazonaws.com',
@@ -6,29 +6,9 @@ const config = {
   password: 'cpsc319aws!',
 };
 
-// function initializeConnection() {
-//   function addDisconnectHandler(connection) {
-//     connection.on('error', function (error) {
-//       if (error instanceof Error) {
-//         if (error.code === 'PROTOCOL_CONNECTION_LOST') {
-//           console.error(error.stack);
-//           console.log('Lost connection. Reconnecting...');
-//           initializeConnection(config);
-//         } else {
-//           throw error;
-//         }
-//       }
-//     });
-//   }
-
-//   const connection = mysql.createConnection(config);
-
 function initializeConnection() {
   const connection = mysql.createPool(config);
   return connection;
 }
 
 module.exports = initializeConnection;
-
-
-
